3fd374e7ed11aef3392ef6512109ec2d04be171b
Timothy Haroutunian <tim@broadinstitute.org>
2012-07-26 10:29:24 -0400

    Added Format Component

--- controllers/components/format.php
+++ controllers/components/format.php
@@ -6,7 +6,13 @@ class FormatComponent extends Object {
   /**
    * PUBLIC METHODS
    */
-  
+
+  public function initialize() {}
+  public function startup() {}
+  public function beforeRender() {}
+  public function beforeRedirect() {}
+  public function shutdown() {}
+
   public function format( $what, $data, $regex = null ) {
     switch( strtolower( $what ) ) {
       case 'phone':
